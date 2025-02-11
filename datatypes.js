c51a8642ced2486ae7ffa9f9f3834f82a2da99ec
lennox <omondilennox9@gmail.com>
2025-02-11 16:56:45 +0300

    Day 1 assignment

---
+++ datatypes.js
@@ -0,0 +1,20 @@
+console.log(typeof 7); // number
+console.log(typeof 'Kenya'); // string
+console.log(typeof true); // boolean
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+// declaring variables in multiple lines 
+
+
+let firstname = 'Lennox'
+let lastname = 'Omondi'
+let maritalStatus = 'Single'
+let country = 'Kenya'
+let age = 23
+
+// declaring variables in a single line
+
+// let firstname = 'Lennox', lastname = 'Omondi', maritalStatus = 'Single', country = 'Kenya', age =23
+
+
---
+++ variable.js
@@ -0,0 +1,8 @@
+// comments can make code readable
+// welcome to 30DaysOfJavaScript
+// comments can make code readable, easy to reuse and informative
+
+let myString = "Hello, World!";
+let myBoolean = true;
+let myUndefined;
+let myNull = null;
